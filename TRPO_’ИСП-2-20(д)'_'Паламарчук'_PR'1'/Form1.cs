3d04f8d27e89bc2e31d04556a8966bbcf6877833
student <student@DESKTOP-CHNTCTT>
2021-09-24 11:20:47 +0300

    Добавьте файлы проекта.

---
+++ TRPO_’ИСП-2-20(д)'_'Паламарчук'_PR'1'/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRPO__ИСП_2_20_д____Паламарчук__PR_1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TRPO_’ИСП-2-20(д)'_'Паламарчук'_PR'1'/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRPO__ИСП_2_20_д____Паламарчук__PR_1_
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
